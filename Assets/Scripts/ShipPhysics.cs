b719293539c0ce256cfb5abf88f0220bed767401
John Li <johnliglobal@gmail.com>
2015-01-18 10:01:16 -0500

    Added Ship-physics prefab

--- Assets/Scripts/ShipPhysics.cs
+++ Assets/Scripts/ShipPhysics.cs
@@ -4,12 +4,11 @@ using System.Collections;
 public class ShipPhysics : MonoBehaviour {
 
 	//Movement
-	public float max_accel_rate = 200;
-	public float max_speed = 40;
-	
-	public float speed = 0;
-	public float accelerationRate = 2;
-	public float deccelerationRate = 3;
+	//public float max_accel_rate = 200;
+	public float max_speed = 200;
+
+	public float accelerationRate = 120;
+	public float deccelerationRate = 80;
 
 	//Thrust Output
 	float MAX_INDIVIDUAL_THRUST = 1;
@@ -27,6 +26,9 @@ public class ShipPhysics : MonoBehaviour {
 	public RSInputAdapter rsInputAdapter;
 
 	float openingSpeedBoost = 100;
+	
+	public Transform[] clampPoints;
+	float clampIntervalSeconds = .5f;
 	// Use this for initialization
 	void Start () {
 		StartCoroutine (CR_WaitForFirstThrust ());
@@ -37,57 +39,38 @@ public class ShipPhysics : MonoBehaviour {
 		isThrusting = false;
 
 		if (Input.GetKey (KeyCode.LeftArrow)) {
-			rigidbody.AddForceAtPosition(leftThruster.forward * speed, leftThruster.position ,  ForceMode.Acceleration);
-			//rigidbody.AddTorque( (leftThruster.position - transform.position).normalized * 5);
 			leftOutput = Mathf.Clamp(leftOutput + outputRate * Time.deltaTime, 0, MAX_INDIVIDUAL_THRUST);
-
 			isThrusting = true;
 		}
 		else if(rsInputAdapter && rsInputAdapter.leftOutputNormalized > 0)
 		{
 			leftOutput = rsInputAdapter.leftOutputNormalized;
-			rigidbody.AddForceAtPosition(leftThruster.forward * speed, leftThruster.position ,  ForceMode.Acceleration);
-			leftOutput = Mathf.Clamp(leftOutput + outputRate * Time.deltaTime, 0, MAX_INDIVIDUAL_THRUST);
-			
 			isThrusting = true;
 		}
 		else
 			leftOutput = Mathf.Clamp(leftOutput - outputDrag * Time.deltaTime, 0, MAX_INDIVIDUAL_THRUST);
 		
 		if (Input.GetKey (KeyCode.RightArrow)) {
-			rigidbody.AddForceAtPosition(rightThruster.forward * speed, rightThruster.position , ForceMode.Acceleration);
 			rightOutput = Mathf.Clamp(rightOutput + outputRate * Time.deltaTime, 0, MAX_INDIVIDUAL_THRUST);
-			//rigidbody.AddTorque(0, -rightOutput * 100, 0);
-
 			isThrusting = true;
 		}
 		else if(rsInputAdapter && rsInputAdapter.leftOutputNormalized > 0)
 		{
 			rightOutput = rsInputAdapter.rightOutputNormalized;		
-			rigidbody.AddForceAtPosition(rightThruster.forward * speed, rightThruster.position , ForceMode.Acceleration);
-			rightOutput = Mathf.Clamp(rightOutput + outputRate * Time.deltaTime, 0, MAX_INDIVIDUAL_THRUST);
-			
 			isThrusting = true;
 		}
 		else 
 			rightOutput = Mathf.Clamp(rightOutput - outputDrag * Time.deltaTime, 0, MAX_INDIVIDUAL_THRUST);
 
-
-		rigidbody.AddRelativeTorque(0, ( leftOutput - rightOutput )*  100, 0);
+		//Turning
+		rigidbody.AddRelativeTorque(0, ( leftOutput - rightOutput ) * 100, 0);
 
 		//Movement
 		if (isThrusting) {
-			//speed = Mathf.Clamp(speed + (leftOutput + rightOutput) * .5f * accelerationRate * Time.deltaTime, 0, max_speed);
 			float acceleration = (leftOutput + rightOutput) * .5f * accelerationRate * Time.deltaTime;
 			rigidbody.velocity += transform.forward * (acceleration );
-
-		} else {
-			//speed = Mathf.Clamp(speed - deccelerationRate * Time.deltaTime, 0, max_speed);
-			//if(rigidbody.velocity.sqrMagnitude > 0)
-			//	rigidbody.velocity -= transform.forward * (deccelerationRate * Time.deltaTime );
-			//Debug.Log(rigidbody.velocity);
-		}
-
+			Debug.Log(rigidbody.velocity.ToString() + "    " + acceleration);
+		} 
 	}
 
 	IEnumerator CR_UpdateLoop()
@@ -110,9 +93,9 @@ public class ShipPhysics : MonoBehaviour {
 
 			yield return 0;	
 		}
-
+		Debug.Log ("ADSOJNE");
 		StartCoroutine (CR_UpdateLoop ());
-		StartCoroutine (CR_OpeningSpeedBurst ());
+		//StartCoroutine (CR_OpeningSpeedBurst ());
 	}
 
 	IEnumerator CR_OpeningSpeedBurst()
@@ -124,21 +107,32 @@ public class ShipPhysics : MonoBehaviour {
 		}
 	}
 
-	void OnCollisionEnter(Collision collision) {
-		int count = 0;
-		float x = 0; float y = 0; float z = 0;
-		foreach (ContactPoint contact in collision.contacts) {
-			count++;
-			x += contact.normal.x;
-			y += contact.normal.y;
-			z += contact.normal.z;
 
-						Debug.DrawRay (contact.point, contact.normal, Color.white);
+	//TODO add masking to ground clamp check
+	IEnumerator CR_GroundClamp()
+	{
+		RaycastHit hit;
+		float x, y, z = 0;
+		while (true) {
+				x = 0;
+				y = 0;
+				z = 0;
+				foreach (Transform tf in clampPoints) {
+						if (Physics.Raycast (transform.position, Vector3.down, out hit, 20)) {
+								Debug.Log ("Hit " + hit.transform.name + "  " + hit.normal);
+								x += hit.normal.x;
+								y += hit.normal.y;
+								z += hit.normal.z;
+						}
+				}
+				x /= clampPoints.Length;
+				y /= clampPoints.Length;
+				z /= clampPoints.Length;
+	
+				//TODO orient myself to the average ground normal. Smooth transition
+				transform.up = new Vector3 (x, y, z);
+				yield return new WaitForSeconds (clampIntervalSeconds);
 		}
-		x /= count;
-		y /= count;
-		z /= count;
-		Vector3 force = new Vector3 (x, 0, z) * collision.relativeVelocity.magnitude  * 5000000;
 	}
 }
 	
